97ca5d123805401883d41f998d804db5952fd1b1
Phil Gebhardt <gebhardt.phil@gmail.com>
2012-11-18 17:21:47 -0800

    Added loose execution functionality. Not perfect..

--- shell.c
+++ shell.c
@@ -2,10 +2,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <string.h>
+
+#define CMD_LEN 100
 
 typedef void (*sighandler_t)(int);
 char c = '\0';
 char* SHELL_TAG = "[SHELL v0.1]";
+
+void call_execve(char *cmd)
+{
+	int i;
+	if(fork() == 0) {
+	
+      /*UNDER CONSTRUCTION*/
+      /*This command works, but takes no parameters or environment variables*/
+		i = execlp(cmd, "", NULL);
+		/*END OF CONSTRUCTION*/
+		
+		if(i < 0)
+		{
+			printf("%s: %s\n", cmd, "command not found");
+			exit(1);		
+		}
+	} else {
+		wait(NULL);
+	}
+}
 
 void handle_signal(int signo) //Handler for CTRL-C signal
 {
@@ -15,18 +38,27 @@ void handle_signal(int signo) //Handler for CTRL-C signal
 
 int main(int argc, char *argv[], char *envp[])  //envp[] = array of environment vars
 {
+   char c;
+   char *cmd = malloc(sizeof(char) * CMD_LEN);
+   
 	//signal(SIGINT, SIG_IGN); //ignores SIGINT signal
-	
 	signal(SIGINT, handle_signal); //SIGINT = signal for CTRL-C
+	
 	printf("%s ", SHELL_TAG);
-	while(c != EOF) //Shell will be terminated when getchar() returns end-of-file (CTRL-D)
+	//Shell will be terminated when getchar() returns end-of-file (CTRL-D)
+	while(c != EOF)
 	{
 		c = getchar();
 		if(c == '\n')
 		{
-		   //Operations start here
-		   printf("Operations start here... \n");
+		   //printf("Operations start here... \n");
+		   call_execve(cmd);
 			printf("%s ", SHELL_TAG);
+			bzero(cmd, CMD_LEN);
+	   }
+	   else
+	   {
+	      strncat(cmd, &c, (size_t) 1);
 	   }
 	}
 	printf("\n");
