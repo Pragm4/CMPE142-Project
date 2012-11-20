836455eab2e0fcdbc0009ea9d7b92e7eb703c190
Phil Gebhardt <gebhardt.phil@gmail.com>
2012-11-19 22:11:04 -0800

    Changed execlp to execve (takes different parameters). Also added function to parse command parameters.

--- shell.c
+++ shell.c
@@ -4,12 +4,62 @@
 #include <signal.h>
 #include <string.h>
 
+/*Definitions*/
 #define CMD_LEN 100
+#define ARGS_LEN 100 //Array of argument strings
+#define ARG_LEN 50   //Single argument string
+#define ENV_LEN 100
 
+/*Type definition for signal handling*/
 typedef void (*sighandler_t)(int);
 
+/*Placeholders*/
 char c = '\0';
-char* SHELL_TAG = "[SHELL v0.1]";
+char *SHELL_TAG = "[SHELL v0.1]";
+char *shellArgs[ARGS_LEN];
+char *shellEnv[ENV_LEN];
+
+/*External Array tracking Environment Variables*/
+extern char **environ;
+
+void populateArgs(char *input)
+{
+   char c;
+   char arg[ARG_LEN];
+   char *temp = input;
+   int i = 0;
+   bzero(arg, ARG_LEN);
+   while(*temp != '\0')
+   {
+      if(*temp == ' ')
+      {
+         if(shellArgs[i] == NULL)
+         {
+            shellArgs[i] = (char*) malloc( sizeof(char)* (strlen(arg) + 1) );
+         }
+         else
+         {
+            bzero(shellArgs[i], strlen(shellArgs[i]));
+         }
+         strncpy(shellArgs[i], arg, strlen(arg));
+         strncat(shellArgs[i], "\0", 1);
+         bzero(arg, ARG_LEN);
+         i++;
+      }
+      else
+      {
+         strncat(arg, temp, 1);
+      }
+      temp++; //increment pointer to string of args (temp[i] = temp[i+1])
+      /*
+      //DEBUGGING PURPOSES: Display arg string by character
+      printf("temp = %c\n", *temp);
+      */
+   }
+   shellArgs[i] = (char*) malloc(sizeof(char)* (strlen(arg) + 1) );
+   strncpy(shellArgs[i], arg, strlen(arg));
+   strncat(shellArgs[i], "\0", 1);
+}
 
 void execute(char *cmd)
 {
@@ -18,15 +68,17 @@ void execute(char *cmd)
 	
       /*UNDER CONSTRUCTION*/
       /*This command works, but takes no parameters or environment variables*/
-		i = execlp(cmd, "", NULL);
+		i = execve(cmd, shellArgs, environ);
 		/*END OF CONSTRUCTION*/
 		
-		if(i < 0)
+		if(i < 0) //Command not found
 		{
 			printf("%s: %s\n", cmd, "*command not found*");
 			exit(1);		
 		}
-	} else {
+	}
+	else //Command found
+	{
 		wait(NULL);
 	}
 }
@@ -39,8 +91,10 @@ void handle_signal(int signo) //Handler for CTRL-C signal
 
 int main(int numArgs, char **args, char **environment)
 {
+   int i;
    char c;
-   char *cmd = malloc(sizeof(char) * CMD_LEN);
+   char *cmd = (char*)malloc(sizeof(char) * CMD_LEN);
+   char *temp = (char*)malloc( sizeof(char) * (CMD_LEN+ARGS_LEN) );
    
 	//signal(SIGINT, SIG_IGN); //ignores SIGINT signal
 	signal(SIGINT, handle_signal); //SIGINT = signal for CTRL-C
@@ -52,14 +106,26 @@ int main(int numArgs, char **args, char **environment)
 		c = getchar();
 		if(c == '\n')
 		{
-		   //printf("Operations start here... \n");
+		   /*Populates shellArgs with parameters within temp*/
+		   populateArgs(temp);
+		   
+		   /*places first argument into command placeholder*/
+		   strcpy(cmd, shellArgs[0]);
+		   
+		   /*Call to fork and execlp with input command*/
 		   execute(cmd);
+		   
+			/*
+			//DEBUGGING PURPOSES: displays shellArgs
+			for(i = 0; shellArgs[i] != NULL; i++) printf("%d: %s ", i, shellArgs[i]);
+			printf("\n");
+			*/
 			printf("%s ", SHELL_TAG);
 			bzero(cmd, CMD_LEN);
 	   }
 	   else
 	   {
-	      strncat(cmd, &c, (size_t) 1);
+	      strncat(temp, &c, (size_t) 1);
 	   }
 	}
 	printf("\n");
